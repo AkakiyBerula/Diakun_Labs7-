24f91767a7d9a53514da8ba80c73ec9387848282
AkakiyBerula <vlad5dyakun@gmail.com>
2021-12-01 21:38:16 +0200

    lab8 task1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask, flash, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+migrate = Migrate(app, db)
+db.init_app(app)
+
+def create_app():
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return app
 
 from . import views, models
